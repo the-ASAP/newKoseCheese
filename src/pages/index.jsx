73a5bfd2228761f7b9f6bd1cf90a6b41f7b78db7
Pavel <buteafulspace@gmail.com>
2021-10-29 18:30:55 +0400

    window debugg

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -39,8 +39,10 @@ const Index = ({ promoContent, discountProduct, categories, posts, newProducts }
         cookiesModal.showModal();
       }
     }, 1000);
+    console.log(window.location?.search);
     if (window && window.location?.search && window.location.search.includes('bxOrderId'))
       // убрал текст из модалки временно
+
       dispatch(
         successPurchasePopupChangeState({
           visible: true,
