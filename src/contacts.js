aa65a21f5202f56bc52a13f29b11a94fb775a1d7
Evstratenko98 <evstratenko.sv98@gmail.com>
2021-11-17 16:40:45 +0400

    new val

--- src/contacts.js
+++ src/contacts.js
@@ -4,3 +4,5 @@ export const email = 'sales@koico.ru';
 export const telegram = 'https://t.me/KoiCoCompany';
 export const adress = `Смоленская область, Кардымовский район, село Каменка ООО "Красная горка", ОГРН
 1136733008763, ИНН 6722041039`;
+
+export const val = '123'
