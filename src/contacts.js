d8cf2f76cb28a4fa3f0631975bff72c3ea2afdae
Pavel <buteafulspace@gmail.com>
2021-11-17 16:56:37 +0400

    fix val

--- src/contacts.js
+++ src/contacts.js
@@ -4,5 +4,3 @@ export const email = 'sales@koico.ru';
 export const telegram = 'https://t.me/KoiCoCompany';
 export const adress = `Смоленская область, Кардымовский район, село Каменка ООО "Красная горка", ОГРН
 1136733008763, ИНН 6722041039`;
-
-export const val = '123'
