9db2a2d79984b0add2d7b0c87eef797fc588fade
Evstratenko98 <evstratenko.sv98@gmail.com>
2022-01-20 13:13:24 +0300

    text in submenu buttons

--- src/components/common/Submenu/Submenu.jsx
+++ src/components/common/Submenu/Submenu.jsx
@@ -39,27 +39,34 @@ export const Submenu = () => {
       <Link href="/products">
         <a className={s.button}>
           <ProductsIcon/>
+          <span className={s.text}>Каталог</span>
         </a>
       </Link>
       <Link href="/login">
         <a className={s.button} onClick={() => dispatch(closeAllModals())}>
           <ProfileIcon/>
+          <span className={s.text}>Профиль</span>
         </a>
       </Link>
       <button type="button"
               onClick={cartModalHandler} className={s.button}>
         <CartIconMobile/>
+        <span className={s.text}>Корзина</span>
       </button>
       <button type="button"
               onClick={favoriteModalHandler} className={s.button}>
         <FavoriteIconMobile/>
+        <span className={s.text}>Избранное</span>
       </button>
       <button type="button"
               onClick={menuModalHandler}
               className={clsx(s.button, s.burger, menuModalValue && s.burgerOpen)}>
-         <span className={s.line}>
-                <span/>
-         </span>
+         <svg width="1.5rem" height="0.75rem" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 1H14" stroke="#FAEEE2" stroke-width="1.24365"/>
+          <path d="M0 6H14" stroke="#FAEEE2" stroke-width="1.24365"/>
+          <path d="M0 11H14" stroke="#FAEEE2" stroke-width="1.24365"/>
+        </svg>
+        <span className={s.text}>Меню</span>
       </button>
     </div>
   );
